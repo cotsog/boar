0e599f53adcc6f9ca2d568805f642a8790328c05
blacksonic <soos.gabor86@gmail.com>
2015-06-27 11:43:44 +0200

    fix if else statement

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
 echo "Starting to deploy"
 
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
   gulp build --production
   cd ./dist
   git init
   git add -A .
   git commit -m "Deploy from build #${TRAVIS_BUILD_NUMBER} commit ${TRAVIS_COMMIT}"
+  git remote add heroku git@heroku.com:boar-online.git
+
+#  gem install heroku
+#  echo "Host heroku.com" >> ~/.ssh/config
+#  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+#  echo "   CheckHostIP no" >> ~/.ssh/config
+#  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+#
+#  heroku keys:clear
+#  yes | heroku keys:add
+#  yes | git push heroku master
 else
   echo "Not possible to deploy"
 fi
