4bcaad3788a649b10f58d0181b56741f24c9d48d
blacksonic <soos.gabor86@gmail.com>
2015-06-27 11:49:37 +0200

    add shebang and git user

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
 echo "Starting to deploy"
 
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
   gulp build --production
   cd ./dist
   git init
+  git config --global user.email "soos.gabor86@gmail.com"
+  git config --global user.name "blacksonic"
   git add -A .
   git commit -m "Deploy from build #${TRAVIS_BUILD_NUMBER} commit ${TRAVIS_COMMIT}"
   git remote add heroku git@heroku.com:boar-online.git
