99f69e643a359b549e64354370ffd90906ef84bd
blacksonic <soos.gabor86@gmail.com>
2015-06-27 12:05:08 +0200

    deploying to heroku

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,15 @@ if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]] ; then
   git commit -m "Deploy from build #${TRAVIS_BUILD_NUMBER} commit ${TRAVIS_COMMIT}"
   git remote add heroku git@heroku.com:boar-online.git
 
-#  gem install heroku
-#  echo "Host heroku.com" >> ~/.ssh/config
-#  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
-#  echo "   CheckHostIP no" >> ~/.ssh/config
-#  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
-#
-#  heroku keys:clear
-#  yes | heroku keys:add
-#  yes | git push heroku master
+  gem install heroku
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+
+  heroku keys:clear
+  yes | heroku keys:add
+  yes | git push heroku master
 else
   echo "Not possible to deploy"
 fi
